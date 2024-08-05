b488a06d15b2c966755c01bc3d29850569ba17c2
GoldenMenina <menezessuraia@gmail.com>
2024-08-05 13:13:47 +0100

    update [id].js

---
+++ pages/api/servicos/[id].js
@@ -0,0 +1,25 @@
+import clientPromise from '../../../lib/mongodb';
+import { ObjectId } from 'mongodb';
+
+export default async function handler(req, res) {
+  const client = await clientPromise;
+  const db = client.db('your_database_name');
+  const collection = db.collection('servicos');
+  const { id } = req.query;
+
+  if (req.method === 'PUT') {
+    const updatedServico = req.body;
+    const result = await collection.findOneAndUpdate(
+      { _id: ObjectId(id) },
+      { $set: updatedServico },
+      { returnOriginal: false }
+    );
+    res.status(200).json(result.value);
+  } else if (req.method === 'DELETE') {
+    await collection.deleteOne({ _id: ObjectId(id) });
+    res.status(204).end();
+  } else {
+    res.setHeader('Allow', ['PUT', 'DELETE']);
+    res.status(405).end(`Method ${req.method} Not Allowed`);
+  }
+}
