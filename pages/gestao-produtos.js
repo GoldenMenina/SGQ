3f2a81fe3408ac0fce64a036a12843ca379d0763
GoldenMenina <menezessuraia@gmail.com>
2024-08-06 21:37:45 +0100

    update gestao-produtos.js

--- pages/gestao-produtos.js
+++ pages/gestao-produtos.js
@@ -67,13 +67,12 @@ const itemsPerPage = 10;
   };
 
   const handleEditProduto = (produto) => {
-    var produtoInfo = produto
-  produtoInfo.quantidade = Number(produtoInfo.quantidade)
-  
-  produtoInfo.preco_venda = Number(produtoInfo.preco_venda)
-  
-  produtoInfo.preco_custo = Number(produtoInfo.preco_custo)
-    setSelectedProduto(produtoInfo);
+    setSelectedProduto({
+      ...produto,
+      quantidade: Number(produto.quantidade),
+      preco_venda: Number(produto.preco_venda),
+      preco_custo: Number(produto.preco_custo)
+    });
     onOpen();
   };
 
@@ -107,7 +106,7 @@ const itemsPerPage = 10;
   
     try {
       if (selectedProduto) {
-        await axios.put(`/api/produtos/${selectedProduto._id}`, produtoInfo);
+        await axios.put(`/api/produtos/${selectedProduto._id}`, produtoData);
         toast({
           title: 'Produto atualizado com sucesso',
           status: 'success',
@@ -206,21 +205,32 @@ const itemsPerPage = 10;
               <FormControl mt={4}>
                 <FormLabel>Quantidade</FormLabel>
                 <NumberInput min={0}>
-                  <NumberInputField name="quantidade" defaultValue={selectedProduto?selectedProduto.quantidade:undefined} required />
+                  <NumberInputField 
+                    name="quantidade" 
+                    value={selectedProduto ? selectedProduto.quantidade : ''}
+                    onChange={(e) => setSelectedProduto({...selectedProduto, quantidade: e.target.value})}
+                    required 
+                  />
                 </NumberInput>
-              </FormControl>
-              <FormControl mt={4}>
-                <FormLabel>Preço de Custo</FormLabel>
-                <NumberInput min={0} precision={2}>
-                  <NumberInputField name="preco_custo" defaultValue={selectedProduto?selectedProduto.preco_custo:undefined} required />
-                </NumberInput>
-              </FormControl>
-              <FormControl mt={4}>
-                <FormLabel>Preço de Venda</FormLabel>
-                <NumberInput min={0} precision={2}>
-                  <NumberInputField name="preco_venda" defaultValue={selectedProduto?selectedProduto.preco_venda:undefined} required />
-                </NumberInput>
-              </FormControl>
+              </FormControl> <
+              FormControl mt = { 4 } >
+                <FormLabel>Preço de Custo</FormLabel> <
+                NumberInput min = { 0 } precision = { 2 } >
+                <NumberInputField 
+                    name="preco_custo" 
+                    value={selectedProduto ? selectedProduto.preco_custo : ''}
+                    onChange={(e) => setSelectedProduto({...selectedProduto, preco_custo: e.target.value})}
+                    required 
+                  /> </NumberInput> </FormControl> <FormControl mt = { 4 } >
+                <FormLabel>Preço de Venda</FormLabel> <
+                NumberInput min = { 0 } precision = { 2 } >
+                <NumberInputField 
+                    name="preco_venda" 
+                    value={selectedProduto ? selectedProduto.preco_venda : ''}
+                    onChange={(e) => setSelectedProduto({...selectedProduto, preco_venda: e.target.value})}
+                    required 
+                  /> </NumberInput>
+                </FormControl>
             </ModalBody>
             <ModalFooter>
                {loading ? (
