99c92a5e98c7f6f2c5ff0b1cd8f53ce61d0848e0
GoldenMenina <menezessuraia@gmail.com>
2024-08-06 09:30:14 +0100

    update [id].js

--- pages/api/clientes/[id].js
+++ pages/api/clientes/[id].js
@@ -7,16 +7,18 @@ export default async function handler(req, res) {
   const collection = db.collection('clientes');
   const { id } = req.query;
 
+  const objectId = new ObjectId(id);
+
   if (req.method === 'PUT') {
     const updatedCliente = req.body;
     const result = await collection.findOneAndUpdate(
-      { _id: ObjectId(id) },
+      { _id: objectId },
       { $set: updatedCliente },
       { returnOriginal: false }
     );
     res.status(200).json(result.value);
   } else if (req.method === 'DELETE') {
-    await collection.deleteOne({ _id: ObjectId(id) });
+    await collection.deleteOne({ _id: objectId});
     res.status(204).end();
   } else {
     res.setHeader('Allow', ['PUT', 'DELETE']);
