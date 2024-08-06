f36a1e56e7793f26730f122bb8965c0b91856496
GoldenMenina <menezessuraia@gmail.com>
2024-08-06 20:08:00 +0100

    update index.js

--- pages/api/empresa/index.js
+++ pages/api/empresa/index.js
@@ -1,18 +1,14 @@
 import clientPromise from '../../../lib/mongodb';
+import { ObjectId } from 'mongodb';
+
 
 export default async function handler(req, res) {
   const client = await clientPromise;
   const db = client.db('sgq');
   const collection = db.collection('empresa');
-
- '66b272905a6d3d8b3bb84412'
-    const page = parseInt(req.query.page) || 1;
-    const limit = parseInt(req.query.limit) || 10;
-    const skip = (page - 1) * limit;
-
-    const funcionarios = await collection.find().skip(skip).limit(limit).toArray();
-    const total = await collection.countDocuments();
-
-    res.status(200).json({ funcionarios, total });
-  
+const objectId = new ObjectId('66b272905a6d3d8b3bb84412')
+  if (req.method === 'GET') {
+    const empresa = await collection.findOne({_id:objectId});
+    res.status(200).json(empresa);
+  }
 }
