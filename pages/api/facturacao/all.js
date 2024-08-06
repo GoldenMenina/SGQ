56856fac7b3ccb69d973f2614a89e616037952a8
GoldenMenina <menezessuraia@gmail.com>
2024-08-06 21:26:34 +0100

    update all.js

--- pages/api/facturacao/all.js
+++ pages/api/facturacao/all.js
@@ -6,10 +6,16 @@ export default async function handler(req, res) {
   const client = await clientPromise;
   const db = client.db('sgq');
  
+const objectId = new ObjectId('66b272905a6d3d8b3bb84412')
 
   if (req.method === 'GET') {
     const servicos = await db.collection('servicos').find({}).toArray()
-    res.status(200).json(empresa);
+    
+      const produtos = await db.collection('produtos').find({}).toArray()
+      
+        const empresa = await db.collection('empresa').findOne({ _id: objectId});
+      const clientes = await db.collection('clientes').find({}).toArray()
+    res.status(200).json({empresa,produtos,servicos,clientes});
   }else if(req.method === 'PUT'){
       const result = await collection.findOneAndUpdate(
       { _id: objectId },
