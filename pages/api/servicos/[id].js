3f8285e1e1db864ae1530f8e33dee9e84becdc51
GoldenMenina <menezessuraia@gmail.com>
2024-08-06 08:17:43 +0100

    update [id].js

--- pages/api/servicos/[id].js
+++ pages/api/servicos/[id].js
@@ -6,15 +6,15 @@ export default async function handler(req, res) {
   const db = client.db('sgq');
   const collection = db.collection('servicos');
   const { id } = req.query;
-console.log(id) 
-return false
   if (req.method === 'PUT') {
     const updatedServico = req.body;
-    const result = await collection.findOneAndUpdate(
+    console.log(ObjectId(id) )
+    const result = await collection.updateOne(
       { _id: ObjectId(id) },
       { $set: updatedServico },
       { returnOriginal: false }
     );
+    console.log(result)
     res.status(200).json(result.value);
   } else if (req.method === 'DELETE') {
     await collection.deleteOne({ _id: ObjectId(id) });
