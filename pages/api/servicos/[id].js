30c47e12c74c31144e6d79fb64c7c0f10c0fdb3f
GoldenMenina <menezessuraia@gmail.com>
2024-08-06 09:28:14 +0100

    update [id].js

--- pages/api/servicos/[id].js
+++ pages/api/servicos/[id].js
@@ -1,47 +1,27 @@
-import { ObjectId } from 'mongodb';
 import clientPromise from '../../../lib/mongodb';
+import { ObjectId } from 'mongodb';
 
 export default async function handler(req, res) {
   const client = await clientPromise;
   const db = client.db('sgq');
   const collection = db.collection('servicos');
   const { id } = req.query;
+  const objectId = new ObjectId(id);
+  if (req.method === 'PUT') {
+    const updatedServico = req.body;
 
-  try {
-    if (req.method === 'PUT') {
-      const updatedServico = req.body;
-      const objectId = new ObjectId(id);
-      console.log('Updating service with ID:', objectId);
-
-      const result = await collection.findOneAndUpdate(
-        { _id: objectId },
-        { $set: updatedServico },
-        { returnDocument: 'after' }  // Use returnDocument instead of returnOriginal
-      );
-
-      if (result.value) {
-        console.log('Update result:', result);
-        res.status(200).json(result.value);
-      } else {
-        res.status(404).json({ error: 'Service not found' });
-      }
-    } else if (req.method === 'DELETE') {
-      const objectId = new ObjectId(id);
-      console.log('Deleting service with ID:', objectId);
-
-      const result = await collection.deleteOne({ _id: objectId });
-
-      if (result.deletedCount === 1) {
-        res.status(204).end();
-      } else {
-        res.status(404).json({ error: 'Service not found' });
-      }
-    } else {
-      res.setHeader('Allow', ['PUT', 'DELETE']);
-      res.status(405).end(`Method ${req.method} Not Allowed`);
-    }
-  } catch (error) {
-    console.error('Error:', error);
-    res.status(500).json({ error: 'Internal Server Error' });
+    const result = await collection.updateOne(
+      { _id: objectId },
+      { $set: updatedServico },
+      { returnOriginal: false }
+    );
+    console.log(result)
+    res.status(200).json(result.value);
+  } else if (req.method === 'DELETE') {
+    await collection.deleteOne({ _id: objectId});
+    res.status(204).end();
+  } else {
+    res.setHeader('Allow', ['PUT', 'DELETE']);
+    res.status(405).end(`Method ${req.method} Not Allowed`);
   }
 }
