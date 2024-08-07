31d5da9776c80871051bdd701d8ccf3a55f87ddf
GoldenMenina <menezessuraia@gmail.com>
2024-08-07 08:50:36 +0100

    update index.js

--- pages/api/login/index.js
+++ pages/api/login/index.js
@@ -10,7 +10,7 @@ export default async function handler(req, res) {
       const { email, password } = req.body;
 
       const user = await db.collection('funcionarios').findOne({ email });
-
+console.log(user)
       if (user && bcrypt.compareSync(password, user.password)) {
         const { password, ...userWithoutPassword } = user;
         res.status(200).json({ success: true, user: userWithoutPassword });
