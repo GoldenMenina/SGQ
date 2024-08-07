49132c4494f87442b8885abf41e00f2cfb44548d
GoldenMenina <menezessuraia@gmail.com>
2024-08-07 08:52:25 +0100

    update index.js

--- pages/api/login/index.js
+++ pages/api/login/index.js
@@ -1,4 +1,4 @@
-// pages/api/login.js
+
 import clientPromise from '../../../lib/mongodb';
 import bcrypt from 'bcryptjs';
 
@@ -6,12 +6,13 @@ export default async function handler(req, res) {
   if (req.method === 'POST') {
     try {
       const client = await clientPromise;
-  const db = client.db('sgq');
+      const db = client.db('sgq');
       const { email, password } = req.body;
 
       const user = await db.collection('funcionarios').findOne({ email });
-console.log(user)
-      if (user && bcrypt.compareSync(password, user.password)) {
+      console.log(user);
+
+      if (user && await bcrypt.compare(password, user.password)) {
         const { password, ...userWithoutPassword } = user;
         res.status(200).json({ success: true, user: userWithoutPassword });
       } else {
